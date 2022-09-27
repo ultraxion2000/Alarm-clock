57e80a223d3c6014db42fc78d00a65d4a2c0881d
ultraxion2000 <sasha.bezobrazov@gmail.com>
2022-09-27 17:27:46 +0300

    Простой будильник

---
+++ Проект 1/Проект 1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Проект_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave_1);
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave_1);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.BackColor = System.Drawing.Color.Ivory;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.BackColor = System.Drawing.Color.MintCream;
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            resources.ApplyResources(this.openFileDialog1, "openFileDialog1");
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Проект 1/Проект 1/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Проект_1
+{
+    public partial class Form1 : Form
+    {
+
+        private int Music = 0;
+        private string NameFile = "";
+
+        private string Hour = "";
+        private string Minutes = "";
+        private string Seconds = "";
+
+        private string HourNow = "";
+        private string MinutesNow = "";
+        private string SecondsNow = "";
+
+        WMPLib.WindowsMediaPlayer WMP = new WMPLib.WindowsMediaPlayer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Interval = 500;
+            timer1.Tick += new EventHandler(Timer1_Tick);
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Hour = DateTime.Now.Hour.ToString();
+            Minutes = DateTime.Now.Minute.ToString();
+            Seconds = DateTime.Now.Second.ToString();
+
+            if (Hour.Length == 1)
+            {
+                Hour = "0" + Hour;
+            }
+
+            if (Minutes.Length == 1)
+            {
+                Minutes = "0" + Minutes;
+            }
+
+            if (Seconds.Length == 1)
+            {
+                Seconds = "0" + Seconds;
+            }
+
+            textBox1.Text = Hour;
+            textBox2.Text = Minutes;
+            textBox3.Text = Seconds;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string extension = "";
+
+            if (Music == 0)
+            {
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    NameFile = openFileDialog1.FileName;
+                    extension = Path.GetExtension(NameFile);
+
+                    if (extension != ".mp3")
+                    {
+                        MessageBox.Show("Файл должен иметь расширение mp3");
+                        return;
+                    }
+
+                    button1.Text = NameFile.Substring(0, 14) + "...";
+                }
+            }
+            else
+            {
+                WMP.controls.stop();
+                button1.Text = NameFile.Substring(0, 14) + "...";
+                Music = 0;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if(button2.Text == "Стоп")
+            {
+                if(Music == 1)
+                {
+                    WMP.controls.stop();
+                    button1.Text = NameFile.Substring(0, 14) + "...";
+                    Music = 0;
+                }
+
+                textBox1.ReadOnly = false;
+                textBox2.ReadOnly = false;
+                textBox3.ReadOnly = false;
+
+                timer1.Enabled = false;
+
+                button2.BackColor = Color.MintCream;
+                button2.Text = "Запустить";
+            }
+            else 
+          { 
+            if(textBox1.Text == "")
+            {
+                MessageBox.Show("Поле часы пусто");
+                    return;
+            }
+
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show("Поле минуты пусто");
+                    return;
+            }
+
+            if (textBox3.Text == "")
+            {
+                MessageBox.Show("Поле секунды пусто");
+                    return;
+            }
+
+            if(!(Convert.ToInt32(textBox1.Text) >= 0 && Convert.ToInt32(textBox1.Text)<= 23))
+                {
+                    MessageBox.Show("Некорректный ввод");
+                    return;
+                }
+
+            if (!(Convert.ToInt32(textBox2.Text) >= 0 && Convert.ToInt32(textBox2.Text) <= 59))
+                {
+                    MessageBox.Show("Некорректный ввод");
+                    return;
+                }
+
+            if (!(Convert.ToInt32(textBox3.Text) >= 0 && Convert.ToInt32(textBox3.Text) <= 59))
+                {
+                    MessageBox.Show("Некорректный ввод");
+                    return;
+                }
+
+
+                if (button1.Text == "Выбрать песню mp3")
+                {
+                    MessageBox.Show("Выберете песню mp3");
+                }
+                else
+                {
+                    button2.BackColor = Color.LavenderBlush;
+                    button2.Text = "Стоп";
+
+                    Hour = textBox1.Text;
+                    Minutes = textBox2.Text;
+                    Seconds = textBox3.Text;
+
+                    textBox1.ReadOnly = true;
+                    textBox2.ReadOnly = true;
+                    textBox3.ReadOnly = true;
+                }
+                timer1.Enabled = true;
+          }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            HourNow = DateTime.Now.Hour.ToString();
+            MinutesNow = DateTime.Now.Minute.ToString();
+            SecondsNow = DateTime.Now.Second.ToString();
+
+            if(HourNow.Length == 1)
+            {
+                HourNow = "0" + HourNow;
+            }
+
+            if (MinutesNow.Length == 1)
+            {
+                MinutesNow = "0" + MinutesNow;
+            }
+
+            if (SecondsNow.Length == 1)
+            {
+                SecondsNow = "0" + SecondsNow;
+            }
+
+            if(Hour == HourNow && Minutes == MinutesNow && Seconds == SecondsNow)
+            {
+                WMP.URL = NameFile;
+                WMP.settings.volume = 100;
+                WMP.controls.play();
+
+                Music = 1;
+
+                button1.Text = "Выключить Музыку";
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar >= '0' && e.KeyChar <= '9')
+            {
+                if(textBox1.Text.Length >=0 && textBox1.TextLength <=1)
+                {
+                    return;
+                }
+            }
+            
+            if(Char.IsControl (e.KeyChar))
+            {
+                if(e.KeyChar == (char) Keys.Back)
+                {
+                    return;
+                }
+            }
+
+            e.Handled = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9')
+            {
+                if (textBox2.Text.Length >= 0 && textBox2.TextLength <= 1)
+                {
+                    return;
+                }
+            }
+
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Back)
+                {
+                    return;
+                }
+            }
+
+            e.Handled = true;
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9')
+            {
+                if (textBox3.Text.Length >= 0 && textBox3.TextLength <= 1)
+                {
+                    return;
+                }
+            }
+
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Back)
+                {
+                    return;
+                }
+            }
+
+            e.Handled = true;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length == 1)
+            {
+                textBox1.Text = "0" + textBox1.Text;
+            }
+        }
+
+        private void textBox2_Leave_1(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length == 1)
+            {
+                textBox2.Text = "0" + textBox2.Text;
+            }
+        }
+
+        private void textBox3_Leave_1(object sender, EventArgs e)
+        {
+            if (textBox3.Text.Length == 1)
+            {
+                textBox3.Text = "0" + textBox3.Text;
+            }
+        }
+
+     
+    }
+}
